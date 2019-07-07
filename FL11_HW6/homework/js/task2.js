e65e752b588df51c7cd9db45217838a10b5750e7
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-07-07 23:41:02 +0300

    Finish commit

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -3,18 +3,17 @@ let secondCatheti = +prompt("Please, enter second catheti", "");
 let hypotenuse = +prompt("Please, enter hypotenuse", "");
 
 if (firstCatheti + secondCatheti > hypotenuse &&
-     firstCatheti + hypotenuse > secondCatheti && secondCatheti + hypotenuse > firstCatheti) {
+    firstCatheti + hypotenuse > secondCatheti && secondCatheti + hypotenuse > firstCatheti) {
     console.log('Triangle can exist');
 
     if (firstCatheti === secondCatheti && secondCatheti === hypotenuse && hypotenuse === firstCatheti) {
         console.log('Eequivalent triangle');
     } else if (firstCatheti === secondCatheti || firstCatheti === hypotenuse || secondCatheti === hypotenuse) {
         console.log('Isosceles triangle');
-    } else if (firstCatheti !== secondCatheti && secondCatheti !== hypotenuse && hypotenuse !== firstCatheti) {
-        console.log('Normal triangle');
     } else {
-        console.log('Triangle doesn’t exist');
+        console.log('Normal triangle');
     }
 } else {
     console.log('Triangle doesn’t exist');
 }
+
